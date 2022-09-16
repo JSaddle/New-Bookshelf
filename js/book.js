79ad5c43db52059fb8f1bc536ff563c922de439a
Jacob Saddler <jacobasaddler@gmail.com>
2022-09-16 10:41:34 -0500

    Final Comments

--- js/book.js
+++ js/book.js
@@ -44,6 +44,9 @@ function Book(author, language, subject, title) {
     });
     //Create a remove Button that allows to us to remove books from our array.
     //I realize this permanently removes the books, and this is NOT what we are looking for, but I
+    //had trouble implementing this feature so that the book still stays in our array on the back end.
+    //I know we would use .splice to remove the books, however I had an issue with putting the function in the right place
+    //so that I could properly refer to the array that the books are in.
 
     const addCom = document.createElement("button");
     const addComInp = document.createElement("input");
@@ -51,18 +54,20 @@ function Book(author, language, subject, title) {
     addCom.textContent = "+";
     addComInp.placeholder = "Add Comment";
     ulBook.append(addCom, addComInp);
-    
+    //Create an Add Comment Button, set max length of comment to 280 characters, append the comment to our Book.
+
+
     addCom.addEventListener("click", function () {
       const comment = document.createElement("li");
       comment.textContent = addComInp.value + " ";
- 
       ulBook.append(comment);
     });
-    
+    //Create an Event Listener so that when we add a comment, our input actually shows up within the Book.
     
     li.append(aT);
     li.append(ulBook);
     return li;
+    //Append all created items to our "li", which is our Book as whole. This way everything is glued together in one place.
   };
 }
 
